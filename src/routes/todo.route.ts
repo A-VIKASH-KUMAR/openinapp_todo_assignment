3fb918a4e91d8b64e40bfa70ce8e757ed417d1ff
A-VIKASH-KUMAR <vickyanga@gmail.com>
2024-02-02 01:20:27 +0530

    feat: :sparkles: add to route to get all tasks list

--- src/routes/todo.route.ts
+++ src/routes/todo.route.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import {
   addTask,
-  addSubTask
+  addSubTask,
+  getAllTasks,
+  getAllSubTasks,
   //   getAllBlogPost,
   //   getBlogPostById,
   //   updateBlogPostById,
@@ -20,7 +22,7 @@ const router = express.Router();
 // task routes
 router.post("/task", validateCreate, addTask);
 router.post("/sub-task", validateCreateSubTask, addSubTask);
-// router.get("/blog",getAllBlogPost);
+router.get("/task",getAllTasks);
 // router.get("/blog/:id", validateGetBlogById, getBlogPostById);
 // router.put("/blog/:id", validateUpdateBlogById, updateBlogPostById);
 // router.delete("/blog/:id", validateDeleteBlogById, deleteBlogPost);
