3781a9701a8b347f6d7ea89d135f74fe840cccc9
A-VIKASH-KUMAR <vickyanga@gmail.com>
2024-02-01 23:01:26 +0530

    feat: :sparkles: add index file

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import todoRoute from "./routes/todo.route";
+import authRoute from "./routes/auth.route"
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+// import cors from "cors";
+dotenv.config();
+const app = express();
+import { connectDb } from "./db";
+import { isAuthenticate } from "./middlewares/isAuthenticate";
+const port = 3001;
+app.use(bodyParser.json());
+
+app.get("/", function (req: any, res: any) {
+  res.send("Hello World");
+});
+
+connectDb().then(() => {
+  app.listen(port, () => {
+    console.log("server is running on port http://localhost:" + port);
+  });
+});
+
+app.use(express.json());
+app.use("/api/auth",  authRoute);
+app.use("/api", isAuthenticate, todoRoute);
