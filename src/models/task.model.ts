9015a3c96ce9108ae528336169f84b47391af02c
A-VIKASH-KUMAR <vickyanga@gmail.com>
2024-02-02 22:05:45 +0530

    feat: :card_file_box: update user type to number instead of object id

--- src/models/task.model.ts
+++ src/models/task.model.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const taskSchema = new mongoose.Schema(
   {
     user: {
-      type: mongoose.Types.ObjectId,
+      type: Number,
       ref: "User",
     },
     title: {
