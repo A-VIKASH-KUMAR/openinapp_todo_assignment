ff18bd99502bfa6f0d99b9b5605842f1cd9ed6aa
A-VIKASH-KUMAR <vickyanga@gmail.com>
2024-02-02 13:49:04 +0530

    feat: :card_file_box: update task model

--- src/models/task.model.ts
+++ src/models/task.model.ts
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const taskSchema = new mongoose.Schema(
   {
-    user:{
-      type:mongoose.Types.ObjectId,
-      ref:"User"
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
     },
     title: {
       type: String,
@@ -14,31 +14,37 @@ const taskSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+
+    priority: {
+      type: Number,
+      enum: [0, 1, 2, 3],
+    },
     due_date: {
       type: Date,
       default: Date.now,
     },
-    priority:{
+    task_id: {
+      unique: true,
       type: Number,
-      enum: [0, 1, 2, 3]
     },
-    task_id:{
-      unique:true,
-      type: Number
-    },
-    status:{
+    status: {
       type: String,
-      enum: ['TODO', 'IN_PROGRESS', 'DONE'],
-      default: 'TODO'
+      enum: ["TODO", "IN_PROGRESS", "DONE"],
+      default: "TODO",
     },
-    subtasks: [{
-      type: Number,
-      ref: 'Subtask',
-    }],
+    subtasks: [
+      {
+        type: Number,
+        ref: "Subtask",
+      },
+    ],
     deleted: {
       type: Boolean,
       default: false,
     },
+    deleted_at: {
+      type: Date,
+    }
   },
   { timestamps: true }
 );
